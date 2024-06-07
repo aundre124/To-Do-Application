5616402d38a7f8676cff29280343f5fefdbd1000
aundre124 <aundre124@gmail.com.com>
2024-06-07 00:16:34 -0400

    Task Completed Change

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,11 +7,13 @@ import { useNavigate } from 'react-router-dom';
 function Home() {
   const [todos, setTodos] = useState([])
   const navigate = useNavigate()
-  
+
+  let user = sessionStorage.getItem("user")
+
   useEffect(() => {
     axios.get('http://localhost:3000/tasks')
-    .then(result => {
-      if (Array.isArray(result.data)) {
+      .then(result => {
+        if (Array.isArray(result.data)) {
           setTodos(result.data)
         }
       })
@@ -21,11 +23,12 @@ function Home() {
 
   const handleDelete = (id) => {
     axios.delete('http://localhost:3000/remove/' + id)
-      .then(result => {
-        location.reload()
+    .then(result => {
+      location.reload()
       })
-      .catch(err => console.log(err))
+    .catch(err => console.log(err))
   }
+
 
   return (
     <div className='flex flex-col w-2/5'>
@@ -40,11 +43,20 @@ function Home() {
           :
           todos.map(todo => (
             <div className='p-2 mt-6 flex flex-col justify-center rounded-lg bg-gray-200' key={todo._id}>
+              <div className='text-xs text-gray-500'>
+                {user}
+                <div className='flex'>
+                <p className='mr-1 italic font-bold'>Task Completed</p>
+                <input className='ml-1' type="checkbox"/>
+                </div>
+              </div>
               <div>
                 {todo.task}
               </div>
-              <div className='flex justify-between text-xs text-gray-500 items-center'>
-                {todo.dueDate}
+              <div className='flex justify-between text-xs text-gray-500 items-center font-bold'>
+                <div className='flex flex-col'>
+                  {todo.dueDate}
+                </div>
                 <div>
                   <i className="bi bi-trash3-fill cursor-pointer ms-3 hover:text-black" onClick={() => handleDelete(todo._id)}></i>
                   <i className="bi bi-pencil-fill cursor-pointer ms-3 hover:text-black"></i>
